16371a5e29c9c2b25275630d682a1e677f920ab7
Erik Unger <erik@erikunger.com>
2018-08-07 12:44:18 +0200

    implement SQL interfaces for uuid.Set

--- set.go
+++ set.go
@@ -2,8 +2,12 @@ package uuid
 
 import (
 	"bytes"
+	"database/sql/driver"
 	"fmt"
 	"sort"
+	"strings"
+
+	"github.com/pkg/errors"
 )
 
 func Less(a, b UUID) bool {
@@ -19,6 +23,10 @@ func Less(a, b UUID) bool {
 	// return false
 }
 
+// Set of UUIDs
+// Is a map[UUID]struct{} underneath.
+// Implements the database/sql.Scanner and database/sql/driver.Valuer interfaces
+// with the nil map value used as SQL NULL
 type Set map[UUID]struct{}
 
 func SetFromSlice(s []UUID) Set {
@@ -89,6 +97,12 @@ func (set Set) Delete(id UUID) {
 	delete(set, id)
 }
 
+func (set Set) DeleteAll() {
+	for id := range set {
+		delete(set, id)
+	}
+}
+
 func (set Set) DeleteSlice(s []UUID) {
 	for _, id := range s {
 		delete(set, id)
@@ -120,4 +134,77 @@ func (set Set) Diff(other Set) Set {
 		}
 	}
 	return diff
+}
+
+// Scan implements the database/sql.Scanner interface
+// with the nil map value used as SQL NULL.
+// Does *set = make(Set) if *set == nil
+// so it can be used with an not initialized Set variable
+func (set *Set) Scan(value interface{}) (err error) {
+	switch x := value.(type) {
+	case string:
+		return set.scanBytes([]byte(x))
+
+	case []byte:
+		return set.scanBytes(x)
+
+	case nil:
+		*set = nil
+		return nil
+	}
+
+	return errors.Errorf("Can't scan value '%#v' of type %T as uuid.Set", value, value)
+}
+
+func (set *Set) scanBytes(src []byte) (err error) {
+	if src == nil {
+		*set = nil
+		return nil
+	}
+
+	if len(src) < 2 || src[0] != '{' || src[len(src)-1] != '}' {
+		return errors.Errorf("Can't parse %#v as uuid.Set", string(src))
+	}
+
+	ids := make([]UUID, 0, 16)
+
+	elements := bytes.Split(src[1:len(src)-1], []byte{','})
+	for _, elem := range elements {
+		elem = bytes.Trim(elem, `'"`)
+		id, err := FromString(string(elem))
+		if err != nil {
+			return err
+		}
+		ids = append(ids, id)
+	}
+
+	if *set == nil {
+		*set = make(Set)
+	} else {
+		set.DeleteAll()
+	}
+	set.AddSlice(ids)
+
+	return nil
+}
+
+// Value implements the driver database/sql/driver.Valuer interface
+// with the nil map value used as SQL NULL
+func (set Set) Value() (driver.Value, error) {
+	if set == nil {
+		return nil, nil
+	}
+
+	var b strings.Builder
+	b.WriteByte('{')
+	for i, id := range set.SortedSlice() {
+		if i > 0 {
+			b.WriteByte(',')
+		}
+		b.WriteByte('\'')
+		b.WriteString(id.String())
+		b.WriteByte('\'')
+	}
+	b.WriteByte('}')
+	return b.String(), nil
 }
